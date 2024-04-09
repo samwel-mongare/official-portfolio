6f9b96785ba0ee515f318ceddb92e5b459eec8d4
mongare <mongaresams@gmail.com>
2024-04-09 09:50:55 +0300

    Update projects

--- app/dataLibrary/myProjects.js
+++ app/dataLibrary/myProjects.js
@@ -1,40 +1,73 @@
-import project1 from '@/assets/images/projects/space.png';
-import project2 from '@/assets/images/projects/bell.png';
-import project3 from '@/assets/images/projects/ace.png';
-import project4 from '@/assets/images/projects/bar.png';
+import project1 from "@/assets/images/projects/dawn.png";
+import project2 from "@/assets/images/projects/bell.png";
+import project3 from "@/assets/images/projects/ace.png";
+// import project4 from "@/assets/images/projects/winlux.png";
+import project5 from "@/assets/images/projects/bar.png";
+import project6 from "@/assets/images/projects/cognipal.png";
+import project7 from "@/assets/images/projects/space.png";
 
 const myProjects = [
   {
-    title: 'Space Travelers Hub',
-    summary: 'A feature-rich  web application that facilitates booking rockets and join selected space missions. It is built for a company that offers both commercial and scientific space travel services. The application also works with real live data from the SpaceX API.',
-    github: 'https://github.com/Mosams/space_travellers',
-    link: 'https://space-traveler-s-hub.netlify.app/',
+    title: "Dawn Property Group",
+    summary:
+      "Dawn Property Group is a real estate website I created as part of a team for a company in Austin, Texas. In this project, I got the opportunity to work with different libraries such as framer motion to create a smooth loading and scrolling effect. One of the features I am most proud of  is the design of the subtle wheel animations which uplift the experience but does not interfer with the core functionality of the web website.",
+    github: "https://github.com/Mosams",
+    link: "https://dawnpropertygroup.com/",
     image: project1,
-    type: 'Featured Project',
+    type: "Featured Project",
   },
   {
-    title: 'Bellfeeds Website',
-    summary: 'A professional portfolio website using React JS, Framer-motion, and Styled-components. It has smooth page transitions, cool background effects, unique design and it is mobile responsive.',
-    github: 'https://github.com/Mosams',
-    link: 'https://conwaybellfield.co.ke/',
+    title: "Bellfeeds Website",
+    summary:
+      "A professional portfolio website using React JS, Framer-motion, and Styled-components. It has smooth page transitions, cool background effects, unique design and it is mobile responsive.",
+    github: "https://github.com/Mosams",
+    link: "https://conwaybellfield.co.ke/",
     image: project2,
-    type: 'Project',
+    type: "Project",
   },
   {
-    title: 'AceWriters Website',
-    summary: 'A JavaScript Quiz App using HTML, CSS, and JavaScript. It has a timer, score counter, and a progress bar. It is mobile responsive.',
-    github: 'https://github.com/Mosams',
-    link: 'https://acewriters254.com/',
+    title: "AceWriters Website",
+    summary:
+      "A landing page that clearly displays the services offers by the client and a design chose by the client. It has a call to action where the user gets to interact with the service provider directly.",
+    github: "https://github.com/Mosams",
+    link: "https://ace-writers.vercel.app/",
     image: project3,
-    type: 'Project',
+    type: "Project",
+  },
+  // {
+  //   title: "Winlux Casino Project",
+  //   summary:
+  //     "This is a project where I really enjoyed and got my hands dirty. I was part of the support team where I developed some sections of the project providing picture perfect replication of the figma files we were given and ensuring responsiveness on every platform ranging from phone, tablets, laptops and computeres while using the latest and most effecient technologies.",
+  //   github: "https://github.com/Mosams",
+  //   link: "https://space-traveler-s-hub.netlify.app/",
+  //   image: project4,
+  //   type: "Featured Project",
+  // },
+  {
+    title: "Space Travelers Hub",
+    summary:
+      "A feature-rich  web application that facilitates booking rockets and join selected space missions. It is built for a company that offers both commercial and scientific space travel services. The application also works with real live data from the SpaceX API.",
+    github: "https://github.com/Mosams/space_travellers",
+    link: "https://space-travellers-delta.vercel.app/",
+    image: project7,
+    type: "Featured Project",
   },
   {
-    title: 'Bar in Motion Website',
-    summary: 'This is a BAR IN MOTION website project created as per the Cindy Shin website design in Behance. The website project is in regard to an event and the website communicates important details about the event and more about the host of the event. (BAR IN MOTION INC.). Built with JavaScript, HTML and CSS. ',
-    github: 'https://github.com/Mosams/Module-One-Capstone-Project',
-    link: 'https://mosams.github.io/Module-One-Capstone-Project/',
-    image: project4,
-    type: 'Featured Project',
+    title: "Bar in Motion Website",
+    summary:
+      "This is a BAR IN MOTION website project created as per the Cindy Shin website design in Behance. The website project is in regard to an event and the website communicates important details about the event and more about the host of the event. (BAR IN MOTION INC.). Built with JavaScript, HTML and CSS. ",
+    github: "https://github.com/Mosams/Module-One-Capstone-Project",
+    link: "https://mosams.github.io/Module-One-Capstone-Project/",
+    image: project5,
+    type: "Project",
+  },
+  {
+    title: "Cognipal Landing Page",
+    summary: "A simple landing page for Cognipal AI company for real estate",
+    github: "https://github.com/Mosams/",
+    link: "https://cognipal.vercel.app/",
+    image: project6,
+    type: "Project",
   },
 ];
 
