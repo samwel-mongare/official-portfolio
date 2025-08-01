37995c4db4b7001ed8f0ea34e472902cf692b2a8
mongare <mongaresams@gmail.com>
2025-08-01 16:06:42 +0300

    Update CV

--- app/dataLibrary/myInformation.js
+++ app/dataLibrary/myInformation.js
@@ -1,70 +1,72 @@
 export const myBiography = [
   `Hi, I'm Mong'are, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
   and user-centered digital experiences. With 4 years of experience in the field. I am always looking for 
-  new and innovative ways to bring my clients' visions to life.`, `I believe that design is about more than just making things look pretty – it's about solving problems and 
-  creating intuitive, enjoyable experiences for users.`, `Whether I'm working on a website, mobile app, or 
+  new and innovative ways to bring my clients' visions to life.`,
+  `I believe that design is about more than just making things look pretty – it's about solving problems and 
+  creating intuitive, enjoyable experiences for users.`,
+  `Whether I'm working on a website, mobile app, or 
   other digital product, I bring my commitment to design excellence and user-centered thinking to 
-  every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.`
-]
+  every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.`,
+];
 
 export const mySkills = [
   {
-    name: 'HTML',
+    name: "HTML",
     x: "-25vw",
     y: "2vh",
   },
   {
-    name: 'CSS',
+    name: "CSS",
     x: "5vw",
     y: "-9vw",
   },
   {
-    name: 'JavaScript',
+    name: "JavaScript",
     x: "20vw",
     y: "6vw",
   },
   {
-    name: 'React',
+    name: "React",
     x: "0vw",
     y: "-22vw",
   },
   {
-    name: 'NextJS',
+    name: "NextJS",
     x: "-20vw",
     y: "-15vw",
   },
   {
-    name: 'TailwindCSS',
+    name: "TailwindCSS",
     x: "15vw",
     y: "-12vw",
   },
   {
-    name: 'Figma',
+    name: "Figma",
     x: "22vw",
     y: "-23vw",
   },
   {
-    name: 'Web Design',
+    name: "Web Design",
     x: "32vw",
     y: "-5vw",
   },
   {
-    name: 'PostgreSQL',
+    name: "Django",
     x: "25vw",
     y: "18vw",
   },
   {
-    name: 'Ruby on Rails',
+    name: "Ruby on Rails",
     x: "-20vw",
     y: "10vw",
   },
   {
-    name: 'NodeJS',
+    name: "NodeJS",
     x: "-10vw",
     y: "25vw",
   },
-  { 
-    name: 'TypeScript',
+  {
+    name: "TypeScript",
     x: "0vw",
     y: "13vw",
   },
@@ -72,77 +74,128 @@ export const mySkills = [
 
 export const myExperience = [
   {
-    title: 'Software Engineer',
-    company: 'BellFeeds',
-    companyLink: 'https://conwaybellfield.co.ke/',
-    location: 'Nairobi, Kenya',
-    date: '2022-Present',
-    description: `Developed and implemented a modern, user-friendly front-end for the official Bellfeeds website using React, Tailwindcss, and Framer Motion.
-    Increased website loading speed by 40% by optimizing code and implementing performance-enhancing techniques.
-    Implemented API calls from the headless CMS eCommerce to link the front end and back end of the website, resulting in a seamless user experience and improved functionality.
-    `
+    title: "Product Manager / Quality Assurance / Tester",
+    company: "SparePap Limited",
+    companyLink: "https://sparepap.com/",
+    location: "Nairobi, Kenya",
+    date: "June 2024 - Present",
+    description: `I led cross-functional collaboration between developers and other departments, reducing feature implementation time 
+    by 30% through streamlined communication and task breakdown. I conducted over 100 whitebox and blackbox tests, ensuring that 95% 
+    of new features were deployed without critical bugs. I managed and prioritized a backlog of more than 50 features and bug fixes,
+     improving development efficiency and reducing turnaround time by 40%. By implementing structured task assignments, I optimized 
+     the development process, reducing miscommunications and rework by 25%. I spearheaded problem-solving sessions with developers, 
+     resolving 90% of blockers before development began, and assisted in prioritizing features based on market research and user feedback, 
+     which led to a 20% increase in user retention through high-impact feature releases. Through rigorous testing and QA processes, 
+     I ensured high product quality, reducing post-release bug reports by 35%.
+    `,
   },
   {
-    title: 'Code Reviewer',
-    company: 'Microverse',
-    companyLink: 'https://www.microverse.org/',
-    location: 'California, United States',
-    date: '2022-Present',
+    title: "Full Stack Web Developer (Contract)",
+    company: "Cognipal",
+    companyLink: "https://cognipal.ai/",
+    location: "Nairobi, Kenya",
+    date: "Mar 2023 - Present",
+    description: `I spearheaded front-end development for a US-based startup, focusing on creating user-friendly interfaces and seamless 
+    user experiences. I participated in developing a real estate AI agent solution that could be easily integrated into websites, enhancing 
+    user engagement and support. I also designed and implemented a user dashboard that allowed clients to manage their accounts, generate 
+    API tokens, and access personalized features. Leveraging technologies such as Puppeteer for web scraping, AWS Lambda functions, and 
+    EC2 instances, I optimized performance and scalability while minimizing costs. I utilized Azure Blob Storage for scraped data, 
+    ensuring efficient data management and retrieval. Throughout the project, I collaborated with cross-functional teams to achieve goals, 
+    following Agile methodologies, documented technical processes, and provided training to team members. My contributions delivered 
+    high-quality, accessible web solutions that supported the company’s growth.
+
+
+
+
+
+
+
+
+Do you like this personality?
+
+
+
+Ask ChatGPT
+
+    `,
+  },
+  {
+    title: "Software Engineer",
+    company: "BellFeeds",
+    companyLink: "https://conwaybellfield.co.ke/",
+    location: "Nairobi, Kenya",
+    date: "Mar 2023 - Present",
+    description: `Developed and implemented a modern, user-friendly front end for the official company website using React, Material-UI, CSS, and Framer Motion.
+Increased website loading speed by 40% by optimizing code and implementing performance-enhancing techniques using server-side rendering privileges of Next.js.
+Implemented API calls from the headless CMS eCommerce to link the website's front and back end, resulting in a seamless user experience and improved functionality.
+Wrote and implemented clean, scalable code based on specifications while complying with WCAG 2.1 accessibility requirements.
+Troubleshooting and debugging code to ensure functionality and accessibility.
+Verified and deployed software systems.
+Maintained software code and security systems.
+Created technical documents and trained staff.
+    `,
+  },
+  {
+    title: "Code Reviewer",
+    company: "Microverse",
+    companyLink: "https://www.microverse.org/",
+    location: "California, United States",
+    date: "Jan 2022 - Mar 2023",
     description: `Provided detailed feedback and suggestions during code reviews, leading to a 50% improvement in coding skills for team members and junior developers.
     Mentored junior developers on best practices for writing clean, efficient code, resulting in a 15% increase in code quality scores.
     Collaborated with the development team on collaborative projects to establish coding standards and guidelines, leading to a 20% improvement in overall code readability.
-    `
+    `,
   },
+  // {
+  //   title: 'Founder/Project Manager',
+  //   company: 'Digital Chicken Farming',
+  //   companyLink: 'https://github.com/Mosams',
+  //   location: 'Eldoret, Kenya',
+  //   date: '2020-2021',
+  //   description: `Implemented an efficient website ordering system that streamlined the purchasing process for customers, resulting in a 50% reduction in order processing time.
+  //   Developed and executed a comprehensive marketing strategy for the free-range chicken project, resulting in a 300% increase in online orders within the first month.
+  //   Collaborated with local farmers and suppliers to source high-quality chicken eggs and roosters, ensuring a consistent supply chain and customer satisfaction.
+  //   `
+  // },
   {
-    title: 'Founder/Project Manager',
-    company: 'Digital Chicken Farming',
-    companyLink: 'https://github.com/Mosams',
-    location: 'Eldoret, Kenya',
-    date: '2020-2021',
-    description: `Implemented an efficient website ordering system that streamlined the purchasing process for customers, resulting in a 50% reduction in order processing time.
-    Developed and executed a comprehensive marketing strategy for the free-range chicken project, resulting in a 300% increase in online orders within the first month.
-    Collaborated with local farmers and suppliers to source high-quality chicken eggs and roosters, ensuring a consistent supply chain and customer satisfaction.
-    `
-  },
-  {
-    title: 'IT Support Technician',
-    company: 'Chebut Tea Factory',
-    companyLink: 'https://ktdateas.com/chebut-tea-factory/',
-    location: 'Kapsabet, Kenya',
-    date: 'June 2019-August 2019',
+    title: "IT Support Technician",
+    company: "Chebut Tea Factory",
+    companyLink: "https://ktdateas.com/chebut-tea-factory/",
+    location: "Kapsabet, Kenya",
+    date: "June 2019-August 2019",
     description: `Conducted troubleshooting of LAN/WAN networks, resolving connectivity issues for 50+ employees and reducing downtime by 30%.
     Performed data entry using the Chai-Pro System with 100% accuracy, ensuring seamless record-keeping and minimizing errors.
     Identified and implemented new software and hardware solutions to improve system performance, resulting in a 20% increase in overall efficiency.
     Identified and implemented software upgrades to improve system performance, resulting in a 20% increase in overall efficiency.
     Conducted extensive testing of new hardware components, leading to the selection and installation of equipment that reduced system downtime by 15%.
-    `
-  }
-]
+    `,
+  },
+];
 
 export const myEducation = [
   {
-    type: 'Full Stack Web Development',
-    time: 'Oct 2021-Sep 2022',
-    place: 'Microverse',
+    type: "Full Stack Web Development",
+    time: "Oct 2021-Sep 2022",
+    place: "Microverse",
     info: `Implemented remote pair programming methodologies using GitHub and industry-standard git-flow to effectively collaborate with international remote developers on a daily basis while ensuring seamless code integration.
     Utilized Ruby, Rails, JavaScript, React, and Redux to develop multiple projects showcasing proficiency in full-stack web development.
     Mastered algorithms, data structures, and full-stack development through 1300+ hours of dedicated study, resulting in a deep understanding of key programming concepts and techniques.
-    `
+    `,
   },
   {
-    type: 'Bachelor in Business Information Technology (Second Class Upper)',
-    time: '2015-2019',
-    place: 'Multimedia University of Kenya',
+    type: "Bachelor in Business Information Technology (Second Class Upper)",
+    time: "2015-2019",
+    place: "Multimedia University of Kenya",
     info: `Utilized my blend of technical knowledge and business acumen to successfully complete projects that required a strong understanding of both fields, resulting in improved efficiency and effectiveness.
     Acquired technical proficiency in networking units, digital logic, and programming languages such as C++ and Java during my studies in Business Information Technology.
     Developed a comprehensive understanding of business operations and management through coursework in accounting, marketing, and business management, enabling me to effectively analyze and address organizational challenges.
-    `
+    `,
   },
   {
-    type: 'Online Coursework',
-    time: '2019-Present',
-    place: 'Coursera And Udemy',
+    type: "Online Coursework",
+    time: "2019-Present",
+    place: "Coursera And Udemy",
     info: `Completed coursework in web development, including HTML, CSS, JavaScript, and React, resulting in a strong foundation in front-end development.
-    Completed coursework in AWS, including EC2, S3, and RDS, resulting in a strong foundation in cloud computing.`
+    Completed coursework in AWS, including EC2, S3, and RDS, resulting in a strong foundation in cloud computing.`,
   },
-]
+];
